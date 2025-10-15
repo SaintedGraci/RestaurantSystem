4a93ae24318d86cc836928b75f54ad9e545afe32
Nigel <r-nigel@chronostep.com>
2025-10-16 06:38:50 +0800

    [Sample] frontend structure

---
+++ restaurant2/resources/views/sample_layout/views/common/_footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer" id="footer_wrapper">
+  <div class="container-fluid clearfix">
+    <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © info@gmail.com {{\Carbon\Carbon::now()->format('Y')}}</span>
+    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="https://google.com/" target="_blank">Sample Page</a></span>
+  </div>
+</footer>
